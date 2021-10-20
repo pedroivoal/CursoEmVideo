84f2ae3688693ad13c01506572078d131ebeace3
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-10-20 09:22:37 -0300

    1°

---
+++ Python/al.19.dicionário/001txt.py
@@ -0,0 +1,12 @@
+filme = {}
+filme['título'] = 'Star Wars'
+filme['ano'] = 1977
+filme['diretor'] = 'George Lucas'
+
+print('values:', filme.values())
+print('key:', filme.keys)
+print('items:', filme.items())
+
+for k,v in filme.items():
+    print(f'O (k) é (v)')
+    
---
+++ Python/al.19.dicionário/002txt.py
@@ -0,0 +1,7 @@
+pessoas = {
+    'nome':'Pedro',
+    'sexo':'M',
+    'idade':18,
+}
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]}')
+print(pessoas.keys())
