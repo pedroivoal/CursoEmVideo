de31849d89157a35b227fd1e192abe18754a7f15
pedro ivo <92477551+Pedro-Ivo-1@users.noreply.github.com>
2021-11-10 14:38:17 -0300

    .

---
+++ Python/al.11.cores.terminal.nt/test2.py
@@ -0,0 +1,4 @@
+n1 = int(input('\033[1;34;47mDigite o 1° número: \033[m'))
+n2 = int(input('\033[1;34;47mDigite o 2° número: \033[m'))
+
+print('\033[1;34;47mVocê digitou \033[32m{} \033[34me \033[32m{}\033[m'.format(n1, n2))
