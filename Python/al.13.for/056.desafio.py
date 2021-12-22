412d0dfce1ecd0b55112bdd9ab21e8f512846c7f
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-22 15:09:59 -0300

    .

---
+++ Python/al.13.for/056.desafio.py
@@ -0,0 +1,23 @@
+num_new_womam = 0
+Sum = 0
+age_older_man = 0
+name_older_man = ''
+
+for i in range(0, 4):
+
+    name = input('\nDigite o nome: ').title()
+    age = int(input('Digite a idade: '))
+    sex = input('Digite o sexo: ').lower()
+
+    Sum += age
+
+    if sex == 'feminino' or sex == 'f' and age < 20:
+        num_new_womam += 1
+
+    if sex == 'masculino' or sex == 'm' and age > age_older_man:
+        age_older_man = age
+        name_older_man = name
+
+print('\nMédia das idades: {}'.format(Sum/4))
+print('Homem mais velho: {}'.format(name_older_man))
+print('Numero de mulheres com menos de 20 anos: {}'.format(num_new_womam))
