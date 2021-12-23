31325422bcdbb5638fe28c787323d7a20754dfed
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-23 20:17:52 -0300

    .

---
+++ Python/al.14.Estrutura de Repetição (while) (cores)/063.Sequência de Fibonacci v1.0.py
@@ -0,0 +1,27 @@
+from colors import color, color_underline
+
+title = color_underline['green']
+answer_col = color['blue']
+error_col = color['red']
+end_col = color['purple']
+clean = color['clean']
+
+print('\n{}Quantos números da Segquência de Fibonacci você deseja ver?{}\n'.format(title, clean))
+n = int(input('Digite um número: '))
+
+i = 0
+tn1 = 1
+tn2 = 1
+sequence = '({}, {}'.format(tn1, tn2)
+while i < n-2:
+    tn3 = tn2 + tn1
+    tn1 = tn2
+    tn2 = tn3
+    sequence += ', {}'.format(tn2)
+
+    i += 1
+
+sequence += ', ...)'
+
+print('\n{1}Segquência de Fibonacci até o {0}º termo.{2}'.format(n, answer_col, clean))
+print('{1}{0}{2}\n'.format(sequence, answer_col, clean))
