2c0ed3fd306fd553eecc09588314c8f3d65a8032
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-25 19:24:43 -0300

    .

---
+++ Python/al.15.Interrompendo repetições (while)/068.Jogo do Par ou Ímpar.py
@@ -0,0 +1,47 @@
+from colors import color, color_underline_bold
+from random import randint
+
+title = color_underline_bold['cyan']
+typed_col = color['green']
+answer_col = color['blue']
+error_col = color['red']
+end_col = color['purple']
+clean = color['clean']
+
+# Título
+print(f'\n{title}Par ou Ímpar{clean}\n')
+
+i = 0
+while True:
+
+    move = input(f'Escolha, par[p] ou ímpar[i]: {typed_col}').strip().lower()
+    print(end = f'{clean}')
+
+    # Garante que valor digitado foi valido
+    while move != 'p' and move != 'i':
+        print('\n{}Valor não aceito.{}'.format(error_col, clean))
+        move = input(f'Escolha, par[p] ou ímpar[i]: {typed_col}').strip().lower()
+        print(end = f'{clean}')
+
+    n = int(input(f'Digite um número: {typed_col}'))
+    print(end = f'{clean}\n')
+
+    n_pc = randint(0,5)
+
+    print(f'Sua jogada: {n}')
+    print(f'Jogada do PC: {n_pc}')
+
+    # Verifica vencedor
+    if (n + n_pc) % 2 == 0 and move == 'p':
+        print(f'\n{answer_col}Você venceu.{clean}\n')
+        i += 1
+    elif (n + n_pc) % 2 == 1 and move == 'i':
+        print(f'\n{answer_col}Você venceu.{clean}\n')
+        i += 1
+    else:
+        print(f'\n{error_col}Você perdeu.{clean}\n')
+        break
+
+# Resutados finais
+print(f'{answer_col}Você venceu {i} veze(s) seguida(s) antes de perder.{clean}')
+print(f'\n{end_col}Fim do programa.{clean}\n')
