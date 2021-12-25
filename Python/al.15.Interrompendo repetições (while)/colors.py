d97fc7cc12b47dd2bf2ddb73e2336ea83d2bbf5e
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-25 19:24:38 -0300

    .

---
+++ Python/al.15.Interrompendo repetições (while)/colors.py
@@ -0,0 +1,54 @@
+# title = color_underline_bold['cyan']
+# typed_col = color['green']
+# answer_col = color['blue']
+# error_col = color['red']
+# end_col = color['purple']
+# clean = color['clean']
+
+color = {
+    'clean': '\033[m',
+    'black': '\033[30m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'purple': '\033[35m',
+    'cyan': '\033[36m',
+    'grey': '\033[37m'
+}
+
+color_bold = {
+    'clean': '\033[m',
+    'grey': '\033[1;30m',
+    'red': '\033[1;31m',
+    'green': '\033[1;32m',
+    'yellow': '\033[1;33m',
+    'blue': '\033[1;34m',
+    'purple': '\033[1;35m',
+    'cyan': '\033[1;36m',
+    'white': '\033[1;37m'
+}
+
+color_underline = {
+    'clean': '\033[m',
+    'black': '\033[4;30m',
+    'red': '\033[4;31m',
+    'green': '\033[4;32m',
+    'yellow': '\033[4;33m',
+    'blue': '\033[4;34m',
+    'purple': '\033[4;35m',
+    'cyan': '\033[4;36m',
+    'white': '\033[4;37m'
+}
+
+color_underline_bold = {
+    'clean': '\033[m',
+    'black': '\033[1;4;30m',
+    'red': '\033[1;4;31m',
+    'green': '\033[1;4;32m',
+    'yellow': '\033[1;4;33m',
+    'blue': '\033[1;4;34m',
+    'purple': '\033[1;4;35m',
+    'cyan': '\033[1;4;36m',
+    'white': '\033[1;4;37m'
+}
