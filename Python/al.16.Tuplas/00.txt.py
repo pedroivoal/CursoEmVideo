c6565c5ef1990f18042e8ad0bf45995bfd089b0f
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-27 09:18:24 -0300

    .

---
+++ Python/al.16.Tuplas/00.txt.py
@@ -0,0 +1,32 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata')
+
+print(lanche, '\n')
+
+for i in lanche:
+    print(f'Eu vou comer {i}')
+print()
+
+for i in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[i]}')
+print()
+
+for pos, element in enumerate(lanche):
+    print(f'Eu vou comer {element}, da posição {pos}.')
+print()
+
+print(sorted(lanche))
+
+a = (1, 2)
+b = (3, 4)
+ab = a + b
+ba = b + a
+print(a)
+print(b)
+print(ab)
+print(ba)
+
+# L = lista qualquer
+# L.count()
+# L.index()
+# L.find()
+# del(L)
