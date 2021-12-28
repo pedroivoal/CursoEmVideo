fc931818ef1f8dd3cc911164850b1ab0c174e488
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-28 18:16:23 -0300

    .

---
+++ Python/al.16.Tuplas/colors.py
@@ -0,0 +1,87 @@
+# title = color_underline_bold['cyan']
+# typed_col = color['green']
+# answer_col = color['blue']
+# fale_col = color['red']
+# error_col = color_underline['red']
+# end_col = color['purple']
+# clean = color['clean']
+
+color = {
+    'clean': '\033[m',
+    'black': '\033[30m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'purple': '\033[35m',
+    'cyan': '\033[36m',
+    'light gray': '\033[37m',
+    'dark gray': '\033[90m',
+    'light red': '\033[91m',
+    'light green': '\033[92m',
+    'light yellow': '\033[93m',
+    'light blue': '\033[94m',
+    'light purple': '\033[95m',
+    'light cyan': '\033[96m',
+    'white': '\033[97m',
+}
+
+color_bold = {
+    'clean': '\033[m',
+    'dark gray': '\033[1;30m',
+    'red': '\033[1;31m',
+    'green': '\033[1;32m',
+    'yellow': '\033[1;33m',
+    'blue': '\033[1;34m',
+    'purple': '\033[1;35m',
+    'cyan': '\033[1;36m',
+    'light gray': '\033[1;37m',
+    'dark gray2': '\033[1;90m',
+    'light red': '\033[1;91m',
+    'light green': '\033[1;92m',
+    'light yellow': '\033[1;93m',
+    'light blue': '\033[1;94m',
+    'light purple': '\033[1;95m',
+    'light cyan': '\033[1;96m',
+    'white': '\033[1;97m',
+}
+
+color_underline = {
+    'clean': '\033[m',
+    'black': '\033[4;30m',
+    'red': '\033[4;31m',
+    'green': '\033[4;32m',
+    'yellow': '\033[4;33m',
+    'blue': '\033[4;34m',
+    'purple': '\033[4;35m',
+    'cyan': '\033[4;36m',
+    'light gray': '\033[4;37m',
+    'dark gray': '\033[4;90m',
+    'light red': '\033[4;91m',
+    'light green': '\033[4;92m',
+    'light yellow': '\033[4;93m',
+    'light blue': '\033[4;94m',
+    'light purple': '\033[4;95m',
+    'light cyan': '\033[4;96m',
+    'white': '\033[4;97m',
+}
+
+color_underline_bold = {
+    'clean': '\033[m',
+    'dark gray': '\033[1;4;30m',
+    'red': '\033[1;4;31m',
+    'green': '\033[1;4;32m',
+    'yellow': '\033[1;4;33m',
+    'blue': '\033[1;4;34m',
+    'purple': '\033[1;4;35m',
+    'cyan': '\033[1;4;36m',
+    'light gray': '\033[1;4;37m',
+    'dark gray2': '\033[1;4;90m',
+    'light red': '\033[1;4;91m',
+    'light green': '\033[1;4;92m',
+    'light yellow': '\033[1;4;93m',
+    'light blue': '\033[1;4;94m',
+    'light purple': '\033[1;4;95m',
+    'light cyan': '\033[1;4;96m',
+    'white': '\033[1;4;97m',
+}
