1da849ada6aa90ee1a539fbf1fa49e6a1e631494
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2021-12-29 11:24:11 -0300

    .

--- Python/al.16.Tuplas/073.Tuplas com Times de Futebol.py
+++ Python/al.16.Tuplas/073.Tuplas com Times de Futebol.py
@@ -7,7 +7,10 @@ clean = color['clean']
 tit = f'{title}Tabela do Brasileirão{clean}'
 print(f'\n{tit:^60}\n')
 
-tabela = ('Atlético-MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians', 'Corinthians', 'Corinthians', 'América-MG', 'Atlético-GO', 'Santos', 'Ceará', 'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude', 'Grêmio', 'Bahia', 'Sport', 'Chapecoense')
+tabela = ('Atlético-MG', 'Flamengo', 'Palmeiras', 'Fortaleza', 'Corinthians',
+'Bull Bragantino', 'Fluminense', 'América-MG', 'Atlético-GO', 'Santos', 'Ceará',
+'Internacional', 'São Paulo', 'Athletico-PR', 'Cuiabá', 'Juventude', 'Grêmio',
+'Bahia', 'Sport', 'Chapecoense')
 
 for i in range(0,5):
     print(f'{answer_col}{i+1}º colocado: {tabela[i]}{clean}')
@@ -16,5 +19,8 @@ for i in range(len(tabela)-4, len(tabela)):
     print(f'{answer_col}{i+1}º colocado: {tabela[i]}{clean}')
 print()
 
-pc = tabela.find('chapecoense')
-print(f'{answer_col}{tabela[pc]} está na {pc}ª posição.{clean}')
+print(sorted(tabela))
+
+
+# pc = tabela.index('chapecoense')
+print(f'\n{answer_col}Chapecoense está na {tabela.index("Chapecoense")+1}ª posição.{clean}')
