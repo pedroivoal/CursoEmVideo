0a631ce9651b0f92413110ded80d2e13b4a84b80
Pedro Ivo <92477551+pedroivoal@users.noreply.github.com>
2022-01-15 19:23:21 -0300

    .

--- Python/al.16.Tuplas/074.Maior e menor valores em Tupla.py
+++ Python/al.16.Tuplas/074.Maior e menor valores em Tupla.py
@@ -1,20 +1,19 @@
-from colors import *
 from random import randint
 
 tup = [randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10)]
 tup = tuple(tup)
-print(tup)
+print(f'\nTupla sorteada: {tup}')
 
 for i in range(0, 5):
     if i == 0:
         maxi = tup[i]
     if tup[i] > maxi:
         maxi = tup[i]
-print(maxi)
+print(f'Maior valor: {maxi}')
 
 for i in range(0, 5):
     if i == 0:
         mani = tup[i]
     if tup[i] < mani:
         mani = tup[i]
-print(mani)
+print(f'Menor valor: {mani}')
